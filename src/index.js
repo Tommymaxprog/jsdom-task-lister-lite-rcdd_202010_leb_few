b0e1983dd58a06d61e7c0f3a28188d4753426aff
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-11-23 09:44:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let input=document.getElementById('new-task-description')
+  let input=document.getElementById('new-task-description');
   let newform=document.getElementById('create-task-form');
   newform.addEventListener("submit", addTask);
 });
 function addTask() {
   event.preventDefault();
+  let input=document.getElementById('new-task-description');
   const newTask = document.createElement("li");
   newTask.innerText = input.value
 }
